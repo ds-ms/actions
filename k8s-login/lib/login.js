d3c20854d695cdc0554e75b5a6d6032e2f73d270
Deepak Sattiraju <desattir@microsoft.com>
2019-07-02 14:41:00 +0530

    Login compile

--- k8s-login/lib/login.js
+++ k8s-login/lib/login.js
@@ -1,11 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = require("@actions/core");
+const command_1 = require("@actions/core/lib/command");
+const path = require("path");
+const fs = require("fs");
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 function run() {
     let kubeconfig = core.getInput('kubeconfig');
     if (!kubeconfig) {
         throw 'Kubeconfig not received';
     }
-    console.log(`kubeconfig: ${kubeconfig}`);
+    const runnerTempDirectory = process.env['RUNNER_TEMPDIRECTORY']; // Using process.env until the core libs are updated
+    const kubeconfigPath = path.join(runnerTempDirectory, `kubeconfig_${getRandomInt(100)}`);
+    core.debug(`Writing kubeconfig contents to ${kubeconfigPath}`);
+    fs.writeFileSync(kubeconfigPath, kubeconfig);
+    command_1.issueCommand('set-env', { name: 'KUBECONFIG' }, kubeconfigPath);
+    console.log('KUBECONFIG environment variable is set');
 }
 run();
