31728a67fec47240344cc046444a42466ae2c558
Deepak Sattiraju <desattir@microsoft.com>
2019-07-02 14:39:06 +0530

    setting kubeconfig

--- k8s-login/src/login.ts
+++ k8s-login/src/login.ts
@@ -1,12 +1,23 @@
 import * as core from "@actions/core";
-let fs = require('fs');
+import { issueCommand } from "@actions/core/lib/command";
+import * as path from 'path';
+import * as fs from 'fs';
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
 function run() {
     let kubeconfig = core.getInput('kubeconfig');
     if (!kubeconfig) {
         throw 'Kubeconfig not received';
     }
-    console.log(`kubeconfig: ${kubeconfig}`);
+    const runnerTempDirectory = process.env['RUNNER_TEMPDIRECTORY']; // Using process.env until the core libs are updated
+    const kubeconfigPath = path.join(runnerTempDirectory, `kubeconfig_${getRandomInt(100)}`);
+    core.debug(`Writing kubeconfig contents to ${kubeconfigPath}`);
+    fs.writeFileSync(kubeconfigPath, kubeconfig);
+    issueCommand('set-env', { name: 'KUBECONFIG' }, kubeconfigPath);
+    console.log('KUBECONFIG environment variable is set');
 }
 
 run();
